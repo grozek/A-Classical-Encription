3abd7b9993aa08f1d637c7be5295b019a0c85abc
grozek <roznawsk@grinnell.edu>
2023-12-09 17:52:56 -0600

    Update documentation, fully working code

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -8,27 +8,34 @@ import java.lang.reflect.Array;
  * typically takes two parameters on the command line. An instruction, contained in args[0], should
  * be either encode or decode. The parameter, contained in args[1], should be the string to encode
  * or decode.
+ * 
+ * CaesarCipher 
+ * Author: Gabriela Roznawska 
+ * CSC207-Fall 2023 
+ * Acknowledgements: Prof. Rebelsky and his resources on the CSC-207 website.
  */
+
 public class CaesarCipher {
     public static void main(String[] args) throws Exception {
         PrintWriter pen = new PrintWriter(System.out, true);
-
         if (args.length != 2) {
             System.err.println("Incorrect number of parameters\n");
         } else if (args[0].equals("encode")) {
             encode(args);
-        }
+            pen.print("\n");
+        } // else if
 
         else if (args[0].equals("decode")) {
             decode(args);
-        }
+            pen.print("\n");
+        } // else if
 
         else {
-            pen.print("Valid options are 'encode' and 'decode' \n");
-        }
+            pen.print("Valid options are 'encode' and 'decode' \n \n");
+        } // else
 
         pen.flush();
-    }
+    }// main(String[] args)
 
     /*
      * Encodes provided string with the Ceasar Cipher encription in 25 different ways
@@ -38,22 +45,21 @@ public class CaesarCipher {
         int len = args[1].length();
         char[] arr = new char[len];
         int n = 0;
-        for (n = 0; n < ('z' - 'a'); n++) {
+        for (n = 0; n < ('z' - 'a' + 1); n++) {
             for (int i = 0; i < len; i++) {
                 int x = args[1].charAt(i) + n;
                 if (x > 'z') {
-                    x = x - ('z' - 'a');
+                    x = x - ('z' - 'a' + 1);
                 }
-
                 char temp = (char) (x);
                 Array.set(arr, i, temp);
-            }
+            } // for
 
             pen.print("n = " + n + ": ");
             pen.print(arr);
             pen.print("\n");
             pen.flush();
-        }
+        } // for
     }// encode
 
     /*
@@ -64,22 +70,19 @@ public class CaesarCipher {
         int len = args[1].length();
         char[] arr = new char[len];
         int n = 0;
-
         for (n = 0; n < 26; n++) {
             for (int i = 0; i < len; i++) {
                 int x = args[1].charAt(i) - n;
                 if (x < 97) {
                     x = x + 26;
-                }
-
+                } // if
                 char temp = (char) (x);
                 Array.set(arr, i, temp);
-            }
-
+            } // for
             pen.print("n = " + n + ": ");
             pen.print(arr);
             pen.print("\n");
             pen.flush();
-        }
+        } // for
     }// decode
 } // CaesarCipher
--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -3,81 +3,88 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.lang.reflect.Array;
 
+
+/*
+ * Encodes and decodes messages using the Vigenere Cipher encryption. The VigenereCipher program
+ * uses keyword to shift the text. It takes three parameters: an instruction, contained in args[0],
+ * should be either encode or decode; the string to encode or decode, and the keyword.
+ * 
+ * VigenereCipher 
+ * Author: Gabriela Roznawska 
+ * CSC207-Fall 2023 
+ * Acknowledgements: Prof. Rebelsky and his resources on the CSC-207 website.
+ */
 public class VigenereCipher {
     public static void main(String[] args) throws Exception {
-        PrintWriter pen = new PrintWriter(System.out, true); 
+        PrintWriter pen = new PrintWriter(System.out, true);
 
         if (args.length != 3) {
-            System.err.println("Incorrect number of parameters\n"); 
+            System.err.println("Incorrect number of parameters\n");
         } else if (args[0].equals("encode")) {
             encode(args);
+            pen.print("\n");
+        } else if (args[0].equals("decode")) {
+            decode(args);
+            pen.print("\n");
+        } else {
+            pen.print("Valid options are 'encode' and 'decode' \n \n");
         }
-
-        else if (args[0].equals("decode")) { 
-            decode(args); 
-        }
-
-        else {
-            pen.print("Valid options are 'encode' and 'decode' \n");
-        }
-
         pen.flush();
+    }// main
 
-    }
-
-    /* Encodes provided string with the Vigenere Cipher encription in 25 different ways
+    /*
+     * Encodes provided string with the Vigenere Cipher encription in 25 different ways
      */
     public static void encode(String[] args) throws Exception {
         PrintWriter pen = new PrintWriter(System.out, true);
-        int lenString = args[1].length(); 
-        int lenKeyword = args[2].length(); 
-        char[] arr = new char[lenString]; 
-        int n = 0; 
+        int lenString = args[1].length();
+        int lenKeyword = args[2].length();
+        char[] arr = new char[lenString];
+        int n = 0;
         int base = 97;
 
         if (lenKeyword == 0) {
-            pen.println(args[1]); 
-        }
-
-        else {
+            pen.println(args[1]);
+        } else {
             for (int i = 0; i < lenString; i++) {
                 n = i % lenKeyword;
                 int x = args[1].charAt(i) + args[2].charAt(n) - base;
-                if (x > 'z')
-                    x = x - ('z' - 'a'); 
+                if (x > 'z') {
+                    x = x - ('z' - 'a' + 1);
+                } // if
+                char temp = (char) (x);
+                Array.set(arr, i, temp);
+            } // for
 
-                char temp = (char) (x); 
-                Array.set(arr, i, temp); 
-            } 
-
-            pen.println(arr); 
+            pen.println(arr);
             pen.flush();
-        }
-    }//encode
+        } // else
+    }// encode(String[] args)
 
-    /* Decodes input provided strings with the Vigenere Cipher encription in 25 different ways
+    /*
+     * Decodes input provided strings with the Vigenere Cipher encription in 25 different ways
      */
     public static void decode(String[] args) throws Exception {
         PrintWriter pen = new PrintWriter(System.out, true);
-        int lenString = args[1].length(); 
-        int lenKeyword = args[2].length(); 
+        int lenString = args[1].length();
+        int lenKeyword = args[2].length();
         char[] arr = new char[lenString];
-        int n = 0; 
-        int x = 0; 
+        int n = 0;
+        int x = 0;
         int base = 97;
         for (int i = 0; i < lenString; i++) {
             n = i % lenKeyword;
             x = args[1].charAt(i) - args[2].charAt(n) + base;
             if (x < base)
-                x = x + ('z' - 'a'); 
-            char temp = (char) (x); 
-            Array.set(arr, i, temp); 
-        } 
-        pen.print(arr); 
+                x = x + ('z' - 'a' + 1);
+            char temp = (char) (x);
+            Array.set(arr, i, temp);
+        } // for
+        pen.print(arr);
         pen.print("\n");
         pen.flush();
-    }//decode
+    }// decode (String[] args)
 
-}//VigenereCipher
+}// VigenereCipher
 
 
